acde41538ca8b20f7c0fca08ff6e47af5dee66fa
ajshemi <akpojotor@yahoo.com>
2019-12-13 10:54:33 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'hello_world', to: 'static#hello_world'  # look for the hello_word action in the StaticController
 end
